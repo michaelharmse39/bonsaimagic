fd9ea7353cf6490ff3bc436fcdcd4599bd800d29
michaelharmse39 <michaelharmse39@gmail.com>
2026-05-01 10:46:16 +0200

    Fix Sanity studio redirect and add sanity as direct dependency

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -1,9 +1,20 @@
 'use client'
 import { NextStudio } from 'next-sanity/studio'
 import config from '../../../../sanity.config'
+import { useEffect } from 'react'
+import { usePathname, useRouter } from 'next/navigation'
 
 export const dynamic = 'force-dynamic'
 
 export default function StudioPage() {
+  const pathname = usePathname()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (pathname === '/studio') {
+      router.replace('/studio/structure')
+    }
+  }, [pathname, router])
+
   return <NextStudio config={config} />
 }
